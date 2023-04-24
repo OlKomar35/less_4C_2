d3af9c7fcb0bbbe6a88d54380e13a68d9c001de0
Olga Komar <komar_ol@mail.ru>
2023-04-24 11:21:58 +0300

    added the solution to the second task

--- Program.cs
+++ Program.cs
@@ -4,3 +4,21 @@
 // 82 -> 10
 // 9012 -> 12
 
+int NumberPrint(string msg){
+    Console.Write($"Введите {msg} =");
+    int res = Convert.ToInt32(Console.ReadLine());
+    return res;
+}
+
+int SumOfNum(int num){
+    int sum = 0;
+    num = Math.Abs(num);
+    while(num > 0){
+        sum = sum + num % 10;
+        num = num / 10;
+    }
+    return sum;
+}
+
+int number = NumberPrint("num");
+Console.WriteLine($"Сумма цифр числа {number} равна {SumOfNum(number)}");
